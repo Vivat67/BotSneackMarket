b6a1c4a114c3cebc764aeeafa359a451d7855578
Vivat67 <131156647+Vivat67@users.noreply.github.com>
2023-08-04 20:50:54 +0300

    Update config_db_snackmart.py

--- config_db_snackmart.py
+++ config_db_snackmart.py
@@ -1,7 +1,1 @@
-# подключаемся к БД на phpMyAdmin
-from peewee import MySQLDatabase
-db = MySQLDatabase(
-    host="server102.hosting.reg.ru",
-    user="u1450880_vlad",
-    password="YourPass123",
-    database="u1450880_vlad")
+
