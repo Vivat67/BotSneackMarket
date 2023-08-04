92bc54debdee0a86dafca348f33052bed88c9943
Vivat67 <131156647+Vivat67@users.noreply.github.com>
2023-08-04 20:48:37 +0300

    comment

--- data_base_snackmart.py
+++ data_base_snackmart.py
@@ -22,7 +22,8 @@ def get_cdek_id_by_user_id(user_id):
     order = Orders.get_or_none(Orders.user_id == user_id)
     # Закрываем соединение с базой данных
     db.close()
-    # Возвращаем найденное значение track_number_CDEK или None, если запись не найдена
+    # Возвращаем найденное значение track_number_CDEK или None,
+    # если запись не найдена
     if order:
         return order.track_number_CDEK
     else:
